452be48bcf9d15a6c93fb85504d3a1b3dbc595f6
JalilRT <41593739+JalilRT@users.noreply.github.com>
2025-02-18 10:27:13 -0600

    update

--- code/RABIES/LME_SB/models/M4H_fc_dataset_left_presubiculum_left_Pmod6.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --time=03:30:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=80
-#SBATCH --account=rrg-mchakrav-ab
-
-singularity exec -B /scratch:/scratch /scratch/m/mchakrav/jrasgado/sudmex_alcohol_rat/analysis/fmri/container/afni.sif  3dLMEr -prefix /scratch/m/mchakrav/jrasgado/sudmex_alcohol_rat/analysis/fmri/Seed_based/models/M4H_fc_dataset_left_presubiculum_left_Pmod6.nii.gz -jobs 1 	-mask /scratch/m/mchakrav/jrasgado/sudmex_alcohol_rat/analysis/fmri/Seed_based/mask_registered.nii.gz         -model 'poly(Age,2)*Intake*Sex+Batch+(1|Subj)'         -qVars 'Age'         -gltCode interaction 'Intake : 1*High -1*Ctrl Sex : 1*male -1*female'         -dataTable @/scratch/m/mchakrav/jrasgado/sudmex_alcohol_rat/analysis/fmri/code/LME_SB/fc_dataset_left_presubiculum_left_Pmod6.txt
-
